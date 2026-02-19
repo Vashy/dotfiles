9fc19a445ad2a6629d4228ab9c4ef10d09910273
Timoty Granziero <timoty.granziero@protonmail.com>
2026-02-20 00:23:55 +0100

    chore: sync

--- dot_config/nvim/lua/custom/plugins/oil.lua
+++ dot_config/nvim/lua/custom/plugins/oil.lua
@@ -1,28 +1,34 @@
 return {
-	"stevearc/oil.nvim",
-	---@module 'oil'
-	---@type oil.SetupOpts
-	opts = {},
-	-- Optional dependencies
-	dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-	config = function()
-		require("oil").setup({
-			default_file_explorer = true,
-			delete_to_trash = true,
-			skip_confirm_for_simple_edits = true,
-			view_options = {
-				show_hidden = true,
-				natural_order = true,
-				is_always_hidden = function(name, _)
-					return name == ".."
-				end,
-			},
-			win_options = {
-				wrap = true,
-			},
-		})
-	end,
-	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-	lazy = false,
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  -- Optional dependencies
+  dependencies = { { 'nvim-mini/mini.icons', opts = {} } },
+  config = function()
+    require('oil').setup {
+      default_file_explorer = true,
+      delete_to_trash = true,
+      skip_confirm_for_simple_edits = true,
+      columns = {
+        'icon',
+        'permissions',
+        'size',
+        'mtime',
+      },
+      view_options = {
+        show_hidden = true,
+        natural_order = true,
+        is_always_hidden = function(name, _)
+          return name == '..'
+        end,
+      },
+      win_options = {
+        wrap = true,
+      },
+    }
+  end,
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
 }
